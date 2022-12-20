8d0bbbf946c3fa922c9b3595f159a56444a14021
MrRickFt <86430556+MrRickFt@users.noreply.github.com>
2022-12-20 17:00:50 -0500

    Primer commit

---
+++ Cypress/cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  //Cambiando las dimensiones del viewport
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  
+  e2e: {
+    setupNodeEvents(on, config) {
+      
+      
+      
+    },
+    
+      //Excluyendo los test por defecto que trae Cypress
+      excludeSpecPattern:[
+        "**/1-getting-started/*.js",
+        "**/2-advanced-examples/*.js"
+      ]
+    
+  },
+});
